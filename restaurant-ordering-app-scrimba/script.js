08022f7eaf4bbd91db03d8fad0291be3e7b553fd
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-06-27 22:20:14 +0300

    meals added

--- restaurant-ordering-app-scrimba/script.js
+++ restaurant-ordering-app-scrimba/script.js
@@ -1,1 +1,26 @@
-import "./data.js";
+import { menuArray } from './data.js';
+
+const mealContainer = document.getElementById('meal-container');
+
+menuArray.forEach(meal => {
+    const mealItem = document.createElement('div');
+    mealItem.classList.add('meal-item');
+
+    mealItem.innerHTML = `
+        <div class="meal-content">
+            <div>
+                <img src="assets/${meal.name.toLowerCase()}.png" alt="${meal.name}">
+            </div>
+            <div>
+                <h4>${meal.name}</h4>
+                <p>${meal.ingredients.join(', ')}</p>
+                <p>$${meal.price}</p>
+            </div>
+        </div>
+        <div>
+            <button><img src="assets/plus.png" alt="Add ${meal.name}"></button>
+        </div>
+    `;
+
+    mealContainer.appendChild(mealItem);
+});
